e9edb7202a68dc5b31f1bc095363337a69930c92
Богдан Гоголь <bogd2288@gmail.com>
2025-01-08 00:38:42 +0300

    refactor: improve fetch instance

--- src/runtime/plugins/api.ts
+++ src/runtime/plugins/api.ts
@@ -1,18 +1,20 @@
-import type { IFetchOptions, IApiHandlers, IApiHandlerTypes } from '../types'
+import type { IApiHandlers, IApiHandlerTypes, IApiPlugin, IFetchOptions } from '../types'
 
 export default defineNuxtPlugin({
   name: 'api',
   enforce: 'pre',
   setup() {
-    const config = useRuntimeConfig()
-    const handlers: IApiHandlers = {
+    const globalHandlers: IApiHandlers = {
       onRequest: [],
       onRequestError: [],
       onResponse: [],
       onResponseError: [],
     }
 
-    function callMaybeArray<T extends (arg: any) => void>(maybeArray: T | T[] | undefined, arg: Parameters<T>[0]) {
+    function callMaybeArray<T extends (arg: any) => void>(
+      maybeArray: T | T[] | undefined,
+      arg: Parameters<T>[0],
+    ): void {
       if (Array.isArray(maybeArray)) {
         maybeArray.forEach(fn => fn(arg))
       }
@@ -21,62 +23,67 @@ export default defineNuxtPlugin({
       }
     }
 
-    const fetch = $fetch.create({
-      baseURL: config.public.baseURL,
-      onRequest: (context: IApiHandlerTypes['onRequest']) => {
-        handlers.onRequest.forEach(handler => handler(context))
-      },
-      onRequestError: (error: IApiHandlerTypes['onRequestError']) => {
-        handlers.onRequestError.forEach(handler => handler(error))
-      },
-      onResponse: (context: IApiHandlerTypes['onResponse']) => {
-        handlers.onResponse.forEach(handler => handler(context))
-      },
-      onResponseError: (error: IApiHandlerTypes['onResponseError']) => {
-        handlers.onResponseError.forEach(handler => handler(error))
-      },
-    })
-
-    function addHandler<K extends keyof IApiHandlers>(type: K, handler: (context: IApiHandlerTypes[K]) => void) {
-      handlers[type].push(handler)
+    function enhanceInstance(instance: typeof $fetch, handlers: IApiHandlers = globalHandlers): IApiPlugin {
+      return Object.assign(
+        <T>(url: string, options?: IFetchOptions): Promise<T> => {
+          return instance<T>(url, {
+            ...options,
+            onRequest: (context) => {
+              handlers.onRequest.forEach(handler =>
+                handler(context as IApiHandlerTypes['onRequest']),
+              )
+              callMaybeArray(options?.onRequest, context)
+            },
+            onRequestError: (error) => {
+              handlers.onRequestError.forEach(handler =>
+                handler(error as IApiHandlerTypes['onRequestError']),
+              )
+              callMaybeArray(options?.onRequestError, error)
+            },
+            onResponse: (context) => {
+              handlers.onResponse.forEach(handler =>
+                handler(context as IApiHandlerTypes['onResponse']),
+              )
+              callMaybeArray(options?.onResponse, context)
+            },
+            onResponseError: (error) => {
+              handlers.onResponseError.forEach(handler =>
+                handler(error as IApiHandlerTypes['onResponseError']),
+              )
+              callMaybeArray(options?.onResponseError, error)
+            },
+          })
+        },
+        {
+          addHandler<K extends keyof IApiHandlers>(
+            type: K,
+            handler: (context: IApiHandlerTypes[K]) => void,
+          ): void {
+            handlers[type].push(handler)
+          },
+          removeHandler<K extends keyof IApiHandlers>(
+            type: K,
+            handler: (context: IApiHandlerTypes[K]) => void,
+          ): void {
+            const index = handlers[type].indexOf(handler)
+            if (index !== -1) handlers[type].splice(index, 1)
+          },
+          create(...args: Parameters<typeof instance.create>): IApiPlugin {
+            const newInstance = instance.create(...args)
+            return enhanceInstance(newInstance, {
+              onRequest: [],
+              onRequestError: [],
+              onResponse: [],
+              onResponseError: [],
+            })
+          },
+        },
+      )
     }
 
-    function removeHandler<K extends keyof IApiHandlers>(type: K, handler: (context: IApiHandlerTypes[K]) => void) {
-      const index = handlers[type].indexOf(handler)
-      if (index !== -1) {
-        handlers[type].splice(index, 1)
-      }
-    }
-
-    function api<T>(url: string, options?: IFetchOptions) {
-      return fetch<T>(url, {
-        ...options,
-        onRequest: (context) => {
-          handlers.onRequest.forEach(handler => handler(context as IApiHandlerTypes['onRequest']))
-          callMaybeArray(options?.onRequest, context)
-        },
-        onRequestError: (error) => {
-          handlers.onRequestError.forEach(handler => handler(error as IApiHandlerTypes['onRequestError']))
-          callMaybeArray(options?.onRequestError, error)
-        },
-        onResponse: (context) => {
-          handlers.onResponse.forEach(handler => handler(context as IApiHandlerTypes['onResponse']))
-          callMaybeArray(options?.onResponse, context)
-        },
-        onResponseError: (error) => {
-          handlers.onResponseError.forEach(handler => handler(error as IApiHandlerTypes['onResponseError']))
-          callMaybeArray(options?.onResponseError, error)
-        },
-      })
-    }
-
-    // Expose the api function directly along with handlers management
     return {
       provide: {
-        api: Object.assign(api, {
-          addHandler,
-          removeHandler,
-        }),
+        api: enhanceInstance($fetch),
       },
     }
   },
